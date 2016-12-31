867a79b98d8f4a84d06b184ac71cdfa207dcc87b
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2016-12-31 01:39:48 +0000

    MNT: Updated the feedstock for conda-smithy version 1.7.0.

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -41,11 +41,7 @@ conda clean --lock
 conda install --yes --quiet conda-forge-build-setup
 source run_conda_forge_build_setup
 
-# Install conda-build 2.x to build a long prefix.
-conda install --yes --quiet conda-build=2
-conda info
-
-# Embarking on 6 case(s).
+# Embarking on 7 case(s).
     set -x
     export CONDA_NPY=110
     export CONDA_PY=27
@@ -88,6 +84,10 @@ conda info
     conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 
-# Inspect the prefix lengths of the built packages.
-conda inspect prefix-lengths /feedstock_root/build_artefacts/linux-64/*.tar.bz2
+    set -x
+    export CONDA_NPY=111
+    export CONDA_PY=36
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 EOF
