78e80c7ed70437c824e616a53c43a1017bf2d038
Filipe Fernandes <ocefpaf@gmail.com>
2018-12-02 17:12:40 -0200

    try without pip

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,15 @@
 :: There is no `gdal-config` on Windows so we need to hardcode gdal's version.
 set GDAL_VERSION=2.2.3
 
-"%PYTHON%" -m pip install --no-deps --ignore-installed . ^
-                          --global-option=build_ext ^
-                          --global-option="-I%LIBRARY_INC%" ^
-                          --global-option="-L%LIBRARY_LIB%" ^
-                          --global-option="-lgdal_i"
+REM For some reason pip is failing
+REM See https://circleci.com/gh/conda-forge/rasterio-feedstock/474?utm_campaign=vcs-integration-link&utm_medium=referral&utm_source=github-build-link
+:: "%PYTHON%" -m pip install --no-deps --ignore-installed . ^
+::                           --global-option=build_ext ^
+::                           --global-option="-I%LIBRARY_INC%" ^
+::                           --global-option="-L%LIBRARY_LIB%" ^
+::                           --global-option="-lgdal_i"
+:: if errorlevel 1 exit 1
+
+
+"%PYTHON%" setup.py build_ext -I"%LIBRARY_INC%" -lgdal_i -L"%LIBRARY_LIB%" install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
