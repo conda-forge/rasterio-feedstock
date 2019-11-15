f9c8509fa03f3ea634d4531f9112edbc31b80922
Filipe Fernandes <ocefpaf@gmail.com>
2019-11-15 16:09:35 -0300

    update gdal ver

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 :: There is no `gdal-config` on Windows so we need to hardcode gdal's version.
-set GDAL_VERSION=3.0.1
+set GDAL_VERSION=3.0.2
 
 "%PYTHON%" -m pip install --no-deps --ignore-installed . ^
                           --global-option=build_ext ^
