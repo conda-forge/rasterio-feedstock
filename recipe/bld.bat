0a4e154cfa7f44e93cf21eb3c4c0236ae5a8b8c5
Filipe Fernandes <ocefpaf@gmail.com>
2019-11-23 18:03:43 -0300

    don't use pip

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,11 @@
 :: There is no `gdal-config` on Windows so we need to hardcode gdal's version.
 set GDAL_VERSION=2.4.3
 
-"%PYTHON%" -m pip install --no-deps --ignore-installed . ^
-                          --global-option=build_ext ^
-                          --global-option="-I%LIBRARY_INC%" ^
-                          --global-option="-L%LIBRARY_LIB%" ^
-                          --global-option="-lgdal_i" -vv
+REM "%PYTHON%" -m pip install --no-deps --ignore-installed . ^
+REM                           --global-option=build_ext ^
+REM                           --global-option="-I%LIBRARY_INC%" ^
+REM                           --global-option="-L%LIBRARY_LIB%" ^
+REM                           --global-option="-lgdal_i" -vv
+%PYTHON% setup.py install --single-version-externally-managed --record record.txt ^
+                          -I%LIBRARY_INC% -L%LIBRARY_LIB% -lgdal_i
 if errorlevel 1 exit 1
