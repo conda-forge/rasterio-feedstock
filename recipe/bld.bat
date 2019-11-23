be664412122d98d2afbd8cbd4393a4deb22e4057
Filipe Fernandes <ocefpaf@gmail.com>
2019-11-23 19:03:16 -0300

    try gdal matrix on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,11 @@
 :: There is no `gdal-config` on Windows so we need to hardcode gdal's version.
-set GDAL_VERSION=2.4.3
+if "%libgdal%" == "2.4" (
+  echo "set GDAL_VERSION=2.4.3"
+  set GDAL_VERSION=2.4.3
+) else (
+  echo "set GDAL_VERSION=3.0.2"
+  set GDAL_VERSION=3.0.2
+)
 
-REM "%PYTHON%" -m pip install --no-deps --ignore-installed . ^
-REM                           --global-option=build_ext ^
-REM                           --global-option="-I%LIBRARY_INC%" ^
-REM                           --global-option="-L%LIBRARY_LIB%" ^
-REM                           --global-option="-lgdal_i" -vv
-%PYTHON%" setup.py build_ext -I"%LIBRARY_INC%" -lgdal_i -L"%LIBRARY_LIB%" install --single-version-externally-managed --record record.txt
+%PYTHON% setup.py build_ext -I"%LIBRARY_INC%" -lgdal_i -L"%LIBRARY_LIB%" install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
