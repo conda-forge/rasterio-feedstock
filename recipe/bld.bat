739927ce03869fae1b9e422b48b907b16c778409
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-09-17 12:03:01 +0200

    Switch to simpler pip install
    Remove (inaccurate) GDAL_VERSION from Windows install

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,2 @@
-:: There is no `gdal-config` on Windows so we need to hardcode gdal's version.
-if "%libgdal%" == "2.4" (
-  echo "set GDAL_VERSION=2.4.3"
-  set GDAL_VERSION=2.4.3
-) else (
-  echo "set GDAL_VERSION=3.0.2"
-  set GDAL_VERSION=3.0.2
-)
-
 %PYTHON% setup.py build_ext -I"%LIBRARY_INC%" -lgdal_i -L"%LIBRARY_LIB%" install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
