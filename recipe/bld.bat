b140855d240535c951d7981c731dcab7cda63957
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-09-17 13:57:19 +0200

    Set gdal version in Windows build from gdalinfo

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,9 @@
+:: There is no `gdal-config` on Windows so we need figure it out from gdalinfo
+for /F "USEBACKQ tokens=2 delims=, " %%F in (`gdalinfo --version`) do (
+set GDAL_VERSION=%%F
+)
+if errorlevel 1 exit 1
+echo "set GDAL_VERSION=%GDAL_VERSION%"
+
 %PYTHON% setup.py build_ext -I"%LIBRARY_INC%" -lgdal_i -L"%LIBRARY_LIB%" install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
