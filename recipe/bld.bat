201da3a4a25fd79157af8ae243a38f23bee91398
Filipe Fernandes <ocefpaf@gmail.com>
2021-12-02 21:15:40 -0300

    update to latest alpha and sync with master changes

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,9 @@
-:: There is no `gdal-config` on Windows so we need to hardcode gdal's version.
-set GDAL_VERSION=2.2.3
+:: There is no `gdal-config` on Windows so we need figure it out from gdalinfo
+for /F "USEBACKQ tokens=2 delims=, " %%F in (`gdalinfo --version`) do (
+set GDAL_VERSION=%%F
+)
+if errorlevel 1 exit 1
+echo "set GDAL_VERSION=%GDAL_VERSION%"
 
-"%PYTHON%" -m pip install --no-deps --ignore-installed . ^
-                          --global-option=build_ext ^
-                          --global-option="-I%LIBRARY_INC%" ^
-                          --global-option="-L%LIBRARY_LIB%" ^
-                          --global-option="-lgdal_i"
+%PYTHON% setup.py build_ext -I"%LIBRARY_INC%" -lgdal_i -L"%LIBRARY_LIB%" install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-$PYTHON -m pip install --no-deps --ignore-installed . \
-                       --global-option=build_ext \
-                       --global-option="-I$PREFIX/include" \
-                       --global-option="-L$PREFIX/lib" \
-                       --global-option="-lgdal"
+${PYTHON} -m pip install . --no-deps -vv
